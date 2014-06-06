56d6b013b45b149c3e96fa76f6179cb84006fdd4
Russ Gaspard <russg@fullsail.edu>
2014-06-05 21:09:54 -0400

    added first files

---
+++ Week1/TweetMe/TweetMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TweetMe
+//
+//  Created by Russell Gaspard on 6/5/14.
+//  Copyright (c) 2014 Russell Gaspard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Week1/TweetMe/TweetMe/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TweetMe
+//
+//  Created by Russell Gaspard on 6/5/14.
+//  Copyright (c) 2014 Russell Gaspard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
